a68d48402514a2939e3af18b095d3bc0ec71b772
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2019-10-10 16:07:20 -0400

    updates

--- wp-content/themes/precinct/templates/components/title.php
+++ wp-content/themes/precinct/templates/components/title.php
@@ -21,7 +21,6 @@ if (get_post_type() == 'election' && !isset($_GET['edit']) && !isset($_GET['resu
           <span class="h6">Precinct ID:</span> <?php echo $precinct_id; ?>
         </small>
       </h1>
-      <p>Lesson Plans</p>
 
       <?php if (isset($_GET['contest'])) {
         $race = $_GET['contest'];
